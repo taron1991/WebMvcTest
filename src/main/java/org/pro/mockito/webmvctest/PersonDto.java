99c7ad76315ec440e0a132f3db18635e529623ca
taron1991 <84153270+taron1991@users.noreply.github.com>
2023-05-09 14:03:52 +0400

    Update PersonDto.java

--- src/main/java/org/pro/mockito/webmvctest/PersonDto.java
+++ src/main/java/org/pro/mockito/webmvctest/PersonDto.java
@@ -1,1 +1,18 @@
+package org.pro.mockito.webmvctest;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonDto {
+
+
+    private String name;
+    private String surname;
+    private Integer age;
+}
